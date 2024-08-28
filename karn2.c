6049e07a9a6f99f68039a3546c10127ea5fabc75
Karnagarwal37 <karnagarwal105491@gmail.com>
2024-08-28 23:53:27 +0530

    Create karn2.c

---
+++ karn2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//area of square/airthmatic instructions
+int main(){
+    int side;
+    printf("enter the side");
+    scanf("%d",&side);
+    printf("area is %d\n",side *side);
+    float radius;
+    printf("enter the radius");
+    scanf("%f",&radius);
+    printf("area is %f",3.14*radius*radius);
+    printf("%d\n",10%7);
+    int a=5*2/3*2;
+    printf("%d\n",a);
+    
+    return 0;
+    
+    
+}
